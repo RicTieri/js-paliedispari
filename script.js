08204dd454e3f6035ad1301ecc79d0eabc9188c6
RicTieri <tieri.riccardo@gmail.com>
2023-11-20 17:17:05 +0100

    added random number gen function

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // Pari e Dispari:
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+const userGameChoose = prompt('choose odd or even');
+const userGameNumber = parseInt(prompt('choose a number betwen 1 and 5'));
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+function randomNumber(min, max){
+  return Math.floor(Math.random(max - min + 1) + min)
+}
 // Sommiamo i due numeri
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando un'altra funzione)
 // Dichiariamo chi ha vinto.
